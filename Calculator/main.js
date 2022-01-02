6270e64695cdc30d3a5eaf893325f8941fb7d54d
BaharJabrayilova <baharjabrayilova009@gmail.com>
2022-01-02 16:31:15 +0400

    salam

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+function Func(deyisen){
+    black.cavab.value+=deyisen;
+}
+function Beraber(){
+    black.cavab.value=eval(kalkulyator.cavab.value);
+}
+function Clear(){
+    black.cavab.value="";
+}
